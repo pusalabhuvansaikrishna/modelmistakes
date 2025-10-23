31280ab2c29d8d2d54b36d16652dde1717420cf4
Bhuvan Sai Krishna <pusalabhuvansaikrishna@gmail.com>
2025-10-23 14:07:29 +0530

    updated utils

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import main
 def process_url(url):
-    page_no, gt, headings, paragraphs=get_data(url)
+    page_no, gt, headings, paragraphs=main.get_data(url)
     return page_no,{"ground_truth":gt,**dict(zip(headings, paragraphs))}
